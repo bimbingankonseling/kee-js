c4a93ac25c9c67ea0901df00f3444ef77c6e5a5e
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-01-14 15:44:37 +0700

    cb

--- login.js
+++ login.js
@@ -1,34 +1,30 @@
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
-function postWithToken(target_url, data, responseFunction) {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+function postWithToken(target_url, datajson, responseFunction) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: JSON.stringify(data),
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-    fetch(target_url, requestOptions)
-        .then(response => response.text())
-        .then(result => responseFunction(JSON.parse(result)))
-        .catch(error => console.log('error', error));
+  fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => responseFunction(JSON.parse(result)))
+    .catch((error) => console.log("error", error));
 }
 
 const Login = () => {
-
     const target_url = "https://asia-southeast2-global-student-401904.cloudfunctions.net/pasetolog";
-    
     const data = {
         "username": getValue("username"),
         "password": getValue("password"),
     };
-
     postWithToken(target_url, data, responseData);
-
 }
 
 function responseData(result) {
@@ -39,10 +35,10 @@ function responseData(result) {
         Swal.fire({
             icon: 'success',
             title: 'Berhasil Masuk',
-            text: "Selamat Datang di Billblis",
+            text: "Selamat Datang di KeeKonseling",
         }).then(() => {
             // Redirect to the dashboard page
-            window.location.href = " ../dashboard.html";
+            window.location.href = " ./dashboard.html";
         });
     } else {
         // Use SweetAlert for error message
@@ -54,4 +50,5 @@ function responseData(result) {
     }
 }
 
-document.getElementById("button").addEventListener("click", Login);
+
+window.Login = Login;
